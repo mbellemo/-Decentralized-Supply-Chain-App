b18d1d206eec572998c6fc11087f98698f0e4645
mbellemo <Cazzo.82>
2022-08-30 11:43:00 +0200

    Add Rinkeby configuration

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,20 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
     }
   }
 };
