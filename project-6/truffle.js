a13dfc2e105af75194fc841d34693af56d5b3bf2
mbellemo <Cazzo.82>
2022-09-22 09:56:31 +0200

    Update truffle configuration

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -15,6 +15,15 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
       network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      docker: false, // Use a version obtained through docker
     }
   }
 };
